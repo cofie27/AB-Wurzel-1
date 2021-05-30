9c76dbbb371341fb623b3aa633ee3f05583ee2d8
cofie27 <78723197+cofie27@users.noreply.github.com>
2021-05-30 19:27:15 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace AB_Wurzel_1
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("------------------------------------Multiplikation-----------------------------------");
+            Quadratwurzel q1 = new Quadratwurzel();
+            q1.eingabeGanzeZahl();
+            q1.druckeGanzeZahl();
+            double n = 0;
+            double i = 1;
+            while (n >= i * i)
+            {
+                if (n == i * i)
+                {
+                    Console.WriteLine("Die Quadratwurzel von " + n + "" + "ist: " + i);
+                }
+                else
+                {
+                    i++;
+                }
+                Console.WriteLine("Fehler");
+            }     
+        }  
+    }
+}
---
+++ Quadratwurzel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AB_Wurzel_1
+{
+    
+
+    class Quadratwurzel
+    {
+        public double i;
+        public double n;
+        private double eD;
+        private double eM;
+
+        public Quadratwurzel()
+        {
+            i = 1;        
+        }
+
+        public void eingabeGanzeZahl()
+        {
+            Console.WriteLine("Bitte geben Sie eine ganze Zahl ein: ");
+            n = Convert.ToInt32(Console.ReadLine());
+        }
+        public void druckeGanzeZahl()
+        {
+            Console.WriteLine("Ihre Eingabe war: "+n);        
+        }
+    }
+        
+}
